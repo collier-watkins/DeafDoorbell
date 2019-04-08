a63f7fce8128a662692ef3fb15f8321f841b4e7d
collier-watkins <collier.watkins.chc@gmail.com>
2019-04-08 10:52:44 -0500

    Flask progress

--- frontend/main/web-server/app.py
+++ frontend/main/web-server/app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 #Home Page
 @app.route("/")	#Related to website locations
 def hello():	#Returns data for the main home page, should be HTML data
-	return "Hello World!"
+	return "<h1>Home Page</h1>"
+
+
+
+
+
+#Run Flask Application
+if __name__ == '__main__':
+	app.run(debug=True)
