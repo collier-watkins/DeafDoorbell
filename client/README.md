bc4ba4ba56d8efc447a479d8a5054788676ff7ca
collier-watkins <collier.watkins.chc@gmail.com>
2019-04-22 19:27:31 -0500

    Client Socket Test

---
+++ client/README.md
@@ -0,0 +1,12 @@
+
+
+## Tutorials I'm Following
+I2C LCD: ```http://www.circuitbasics.com/raspberry-pi-i2c-lcd-set-up-and-programming/```
+
+
+## Current Plan
+* Auto-run app.py on startup
+* Setup socket communication to server using local IP
+* Constant listen for string comming in from socket
+* Perform appropriate function like display message and await button press
+* Constantly send socket communication about room occupancy sensor
---
+++ client/app.py
@@ -0,0 +1,59 @@
+import I2C_LCD_driver
+import socket
+import fcntl
+import struct
+
+import sys
+from thread import *
+
+from time import *
+
+host = "www.google.com"
+port = 80
+
+
+def get_pi_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+mylcd = I2C_LCD_driver.lcd()
+
+
+
+def setupServer():
+	try: 
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+		print("Socket s created.")
+	except socket.error as msg :
+		print(msg)
+		sys.exit()
+	return s
+
+
+
+###### LCD Setup #############
+print("Starting...")
+print("Current Local IP: " + get_pi_ip_address('wlan0'))
+
+
+mylcd.lcd_display_string("IP:" + get_pi_ip_address('wlan0'), 2, 0)
+
+
+
+####### Socket Setup ######################################
+
+sendableString = "Yo, what's up"
+
+
+
+s = setupServer()
+
+host_ip = socket.gethostbyname(host)
+
+s.connect((host_ip, port))
+
+print("Socket connected")
