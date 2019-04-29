1d8e6ebc2f60f13e94bf758ef9829d6be7e08b32
collier-watkins <collier.watkins.chc@gmail.com>
2019-04-29 10:24:03 -0500

    Web server test

--- crunchTimeBuild/webServerTest.py
+++ crunchTimeBuild/webServerTest.py
@@ -1,12 +1,40 @@
-import http.server
-import socketserver
+#!/usr/bin/env python
+ 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+ 
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  #def openPage(self, filename):
+  #	with open(filename) as f:
+  #		return f.read()
 
-# Web Server Portion #################
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        #message = self.openPage("index.html")
+        message = "Howdy"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
 
-webPORT = 80
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", webPORT), Handler) as httpd:
-    print("web serving at port", webPORT)
-    httpd.serve_forever()
-
+ 
+def run():
+  print('starting server...')
+ 
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+ 
+ 
+run()
