c3eca4b6a5957ba12c57f9e7aba1393d0bfa8762
collier-watkins <collier.watkins.chc@gmail.com>
2019-04-29 20:20:06 -0500

    switched filenames

--- crunchTimeBuild/server.py
+++ crunchTimeBuild/server.py
@@ -1,80 +1,94 @@
-import socket, select
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-import I2C_LCD_driver
-
+import socket
 import fcntl
 import struct
 
-import sys
-from thread import *
+import I2C_LCD_driver
 
-from time import *
+from subprocess import check_output
 
-import RPi.GPIO as GPIO
+host_name = ''  # Change this to your Raspberry Pi IP address
+host_port = 80
 
-######################################
+mylcd = I2C_LCD_driver.lcd()
 
-
-def get_pi_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-       s.fileno(),
-       0x8915,  # SIOCGIFADDR
-       struct.pack('256s', ifname[:15])
-   )[20:24])
-
-if __name__ == "__main__":
-
-	mylcd = I2C_LCD_driver.lcd()
-
-	mylcd.lcd_clear()
-
-	print("Starting...")
-	print("Current Local IP: " + get_pi_ip_address('wlan0'))
-
-	mylcd.lcd_display_string("Server", 1, 0)
-	mylcd.lcd_display_string(get_pi_ip_address('wlan0'), 2, 0)
+lcdClearLine = "                "
 
 
+class MyServer(BaseHTTPRequestHandler):
+    """ A special implementation of BaseHTTPRequestHander for reading data from
+        and control GPIO of a Raspberry Pi
+    """
 
-	CONNECTION_LIST = []
-	RECV_BUFFER = 4096	#4 kb
-	PORT = 8888
+    def openPage(self, filename):
+      with open(filename) as f:
+        return f.read()
 
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server_socket.bind(("", PORT))
-	server_socket.listen(10)	#up to 10 connections
+    def do_HEAD(self):
+        """ do_HEAD() can be tested use curl command
+            'curl -I http://server-ip-address:port'
+        """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-	CONNECTION_LIST.append(server_socket)	#Not sure if neccessary
+    def do_GET(self):
+        """ do_GET() can be tested using curl command
+            'curl http://server-ip-address:port'
+        """
+        html = self.openPage("index.html")
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        status = ''
+        if self.path=='/':
+            #GPIO.setmode(GPIO.BCM)
+            #GPIO.setwarnings(False)
+            #GPIO.setup(18, GPIO.OUT)
+            mylcd.lcd_display_string("*", 1, 15)
 
-	print("Chat server has started on port " + str(PORT))
 
-	while True :
-		#poll here
-		read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST, [], [])
-
-		for sock in read_sockets :
-			if sock == server_socket :	#If the connection is from ourselves
-				sockfd, addr = server_socket.accept()
-				CONNECTION_LIST.append(sockfd)
-				print("Client (%s, %s) connected" % addr)
-				mylcd.lcd_display_string("%", 1, 15)
+        #elif self.path=='/on':
+            #GPIO.output(18, GPIO.HIGH)
+            #status='LED is On'
+        #    mylcd.lcd_display_string("*", 1, 15)
 
 
-			else :
-				try:
-					data = sock.recv(RECV_BUFFER)
-					if data :
-						#Save data.deac
-						sock.send(("Server got: " + data.decode() ).encode())
-						print("Client says:" + data.decode())
-						mylcd.lcd_display_string("reply sent", 1, 1)
-				except:
-					broadcast_data(sock, "Client is offline")
-					print("Client is offline (printstatement)")
-					sock.close()
-					CONNECTION_LIST.remove(sock)
-					continue
+        #elif self.path=='/off':
+            #GPIO.output(18, GPIO.LOW)
+            #status='LED is Off'
+        #    mylcd.lcd_display_string("o", 1, 15)
+        elif self.path.startswith('/sub/'):
+          #print(self.path)
+          msg = self.path[5:].replace("_", " ")
+          print("'" + msg + "'")
+          mylcd.lcd_display_string(lcdClearLine, 2, 0)
+          mylcd.lcd_display_string(msg, 2, 0)
 
-	server_socket.close()
+
+
+        self.wfile.write(html.format(temp[5:], status).encode("utf-8"))
+
+
+if __name__ == '__main__':
+    http_server = HTTPServer((host_name, host_port), MyServer)
+    print("Server Starts - %s:%s" % (host_name, host_port))
+
+    
+
+    mylcd.lcd_clear()
+
+    ip = check_output(['hostname', '--all-ip-addresses']).decode("utf-8").strip()
+    mylcd.lcd_display_string("Server", 1, 0)
+
+    print("'" + ip + "'")
+    mylcd.lcd_display_string(ip, 2, 0)
+    
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
---
+++ crunchTimeBuild/serverOldBuild.py
@@ -0,0 +1,80 @@
+import socket, select
+
+import I2C_LCD_driver
+
+import fcntl
+import struct
+
+import sys
+from thread import *
+
+from time import *
+
+import RPi.GPIO as GPIO
+
+######################################
+
+
+def get_pi_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+       s.fileno(),
+       0x8915,  # SIOCGIFADDR
+       struct.pack('256s', ifname[:15])
+   )[20:24])
+
+if __name__ == "__main__":
+
+	mylcd = I2C_LCD_driver.lcd()
+
+	mylcd.lcd_clear()
+
+	print("Starting...")
+	print("Current Local IP: " + get_pi_ip_address('wlan0'))
+
+	mylcd.lcd_display_string("Server", 1, 0)
+	mylcd.lcd_display_string(get_pi_ip_address('wlan0'), 2, 0)
+
+
+
+	CONNECTION_LIST = []
+	RECV_BUFFER = 4096	#4 kb
+	PORT = 8888
+
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server_socket.bind(("", PORT))
+	server_socket.listen(10)	#up to 10 connections
+
+	CONNECTION_LIST.append(server_socket)	#Not sure if neccessary
+
+	print("Chat server has started on port " + str(PORT))
+
+	while True :
+		#poll here
+		read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST, [], [])
+
+		for sock in read_sockets :
+			if sock == server_socket :	#If the connection is from ourselves
+				sockfd, addr = server_socket.accept()
+				CONNECTION_LIST.append(sockfd)
+				print("Client (%s, %s) connected" % addr)
+				mylcd.lcd_display_string("%", 1, 15)
+
+
+			else :
+				try:
+					data = sock.recv(RECV_BUFFER)
+					if data :
+						#Save data.deac
+						sock.send(("Server got: " + data.decode() ).encode())
+						print("Client says:" + data.decode())
+						mylcd.lcd_display_string("reply sent", 1, 1)
+				except:
+					broadcast_data(sock, "Client is offline")
+					print("Client is offline (printstatement)")
+					sock.close()
+					CONNECTION_LIST.remove(sock)
+					continue
+
+	server_socket.close()
